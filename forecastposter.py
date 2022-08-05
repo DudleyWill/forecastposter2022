0d0420940ae06630348efa8c882e08d83625f393
dataduds <slickmig1@hotmail.com>
2022-08-05 00:12:35 -0500

    added new gile and script updated

--- forecastposter.py
+++ forecastposter.py
@@ -9,32 +9,89 @@ import glob
 import plotly.express as px
 
 # st.set_page_config(age_title='heat-stressed-poultry')
-st.set_page_config(layout="wide")
+# st.set_page_config(layout="wide")
 
 @st.cache
 def readdata(file):
     van20all1 = pd.read_excel(file)
-    time_plot1 = van20all1[['Section4','Section3','index']].set_index('index')
-    time_plot1['deltaT'] = time_plot1['Section4'] -time_plot1['Section3']
+    time_plot1 = van20all1[['Section4','Section3','index','Outdoor']].set_index('index')
+    time_plot1['s3out'] = time_plot1['Section3'] -time_plot1['Outdoor']
+    time_plot1['s4out'] = time_plot1['Section4'] - time_plot1['Outdoor']
     return van20all1, time_plot1
 
 @st.cache
 def loadimage(img):
    return Image.open(img)
 
-fName = "van20jun.xlsx"
+fName = "van20all.xlsx"
 van20all,time_plot = readdata(fName)
 header_container = st.container()
 historical_container = st.container()
 
-with st.sidebar:
-    st.header('Choose File Controls')
+def graphs20 (thi,section):
+    fig1 = px.scatter(van20all, x='date', y='hour', color=thi,
+                     labels={thi: section, 'hour': 'Time', 'date': 'Date'}
+                     , marginal_y="box",
+                     symbol=thi,
+                     category_orders={thi: ['No stress', 'Heat stressed', 'Severely stressed']},
+                     color_discrete_sequence=["#70bb2a", "#ffb814", "#ff1120"], size_max=10,
+                     width=1300, height=600
 
-    with st.expander("TXT Data file from CSGM"):
-        weatherType1 = st.selectbox(
-            'Select Weather variable being processed?',
-            ('Humidity', 'Temperature', 'Rainfall', 'wind speed'))
-        # st.write('You selected:', weatherType1)
+                     )
+    fig1.update_layout(legend=dict(
+        orientation="h",
+        yanchor="top",
+        y=-0.15,
+        xanchor="right",
+        x=1
+    ))
+
+    fig1.update_layout(
+        yaxis=dict(
+            tickmode='array',
+            range=[-0.5, len(van20all.hour.unique()) + 0.5],
+            tickvals=van20all.hour.unique(),
+            ticktext=van20all.time.unique(),
+            # tickfont_family="Arial Black",
+            # tickfont_size=15
+        )
+    )
+    fig1.update_layout(
+        font_family="Arial Black",
+        font_color="white",
+        title_font_family="Times New Roman",
+        title_font_color="white",
+        legend_title_font_color="white",
+        # font_size=15
+    )
+    return  fig1
+
+
+def timeseries(section,outt):
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.01, row_heights=[0.3, 0.7], )
+
+    fig.add_trace(
+        go.Scatter(x=time_plot.index, y=time_plot[section], name='inTemp', line_color='red'),
+        row=2, col=1
+    )
+    fig.add_trace(
+        go.Scatter(x=time_plot.index, y=time_plot['Outdoor'], name='OutTemp', line_color='blue'),
+        row=2, col=1
+    )
+
+    fig.add_trace(
+        go.Scatter(x=time_plot.index, y=time_plot[outt], name='tempDiff'),
+        row=1, col=1
+    )
+
+    fig.update_yaxes(
+        title_text="Temperature \N{DEGREE SIGN} C ",
+        title_standoff=25)
+
+    fig.update_layout(height=600, width=1300,
+                      )
+    return fig
+
 
 with header_container:
     image = loadimage('conference logo2.png')
@@ -48,54 +105,44 @@ with header_container:
     st.title('NEARING A TIPPING POINT: HEAT-STRESS AND SMALL SCALE POULTRY FARMING IN JAMAICA')
     st.subheader('Abstract submitted to the FORECAST Conference 2022')
 
-    with st.expander("View abstract"):
-        st.text(
-            "Background: Heat stress is the leading cause of high mortality and low production in small livestock farming")
-        st.text(
-            "in Jamaica. Additionally, the persistent warming of the Caribbean due to climate change worsens the problem")
-        st.text(
-            "in the sector. This paper presents a field investigation into the heat stress problem at the Vere Technical")
-        st.text("High School in Clarendon Jamaica")
+    with st.expander("Research Background"):
+        st.write(
+            "Heat stress is the leading cause of high mortality and low production in small livestock farming in"
+            "   Jamaica. Additionally, the persistent warming of the Caribbean due to climate change worsens the problem"
+            "in the sector. This paper presents a field investigation into the heat stress problem at the Vere Technical"
+            "High School in Clarendon Jamaica")
 
 with historical_container:
-    with st.expander(" Interactive graphs"):
-        fig = px.scatter(van20all, x='date', y='hour', color='Sec3THI',
-                         labels={'Sec3THI': 'Section3', 'hour': 'Time', 'date': 'Date'}
-                         , marginal_y="box",
-                         symbol='Sec3THI',
-                         category_orders={'Sec3THI': ['No stress', 'Heat stressed', 'Severely stressed']},
-                         color_discrete_sequence=["green", "blue", "red"], size_max=10,
-                         width=1300, height=600
+    with st.expander(" Results"):
+        graphs = st.radio("Interactive graphs ",
+                          ('Section 4 compared with outdoor',
+                           'Section 3 compared with outdoor',
+                           'Section 3 hourly THI',
+                           'Section 4 hourly THI')
+                          ,horizontal=True)
+        if graphs =='Section 4 compared with outdoor':
+            fig = timeseries('Section4', 's4out')
+            st.plotly_chart(fig, use_container_width=True)
+            st.write("Shows the temperature of inside of section 4, where no interventon was done, compared to outdoor temperature. "
+            "Note worthy, section 4 achieved lower temperatures during the night than section 3" )
+        elif graphs=='Section 3 compared with outdoor':
 
-                         )
-        fig.update_layout(legend=dict(
-            orientation="h",
-            yanchor="top",
-            y=-0.15,
-            xanchor="right",
-            x=1
-        ))
+            fig2 = timeseries('Section3', 's3out')
+            st.plotly_chart(fig2, use_container_width=True)
+            st.write(
+                "Compares the temperature of section 3 where the radiant barrier was installed with respect to outdoor temperatures. "
+                "Section 3 maintained a temperature difference of 0\N{DEGREE SIGN} C to 4\N{DEGREE SIGN} C in the daytime while section 4"
+                "could go as high as ~5.6\N{DEGREE SIGN} C.  ")
+        elif graphs =='Section 4 hourly THI':
+            fig = graphs20('Sec4THI', 'Section4')
+            st.plotly_chart(fig, use_container_width=True)
+            st.write(
+                'Shows the hourly variation of the temperature hunmidity index which is used to estimate the level of heat stress expereinced by '
+                'chickens in section 4 of the poultry house')
+        else:
 
-        fig.update_layout(
-            yaxis=dict(
-                tickmode='array',
-                range=[-0.5, len(van20all.hour.unique()) + 0.5],
-                tickvals=van20all.hour.unique(),
-                ticktext=van20all.time.unique(),
-                # tickfont_family="Arial Black",
-                # tickfont_size=15
-            )
-        )
-        fig.update_layout(
-            font_family="Arial Black",
-            font_color="white",
-            title_font_family="Times New Roman",
-            title_font_color="white",
-            legend_title_font_color="white",
-            # font_size=15
-        )
-
-        # fig.update_xaxes(tickfont_family="Arial Black")
-        # fig.update_xaxes(tickfont_size=15, ticks="outside")
-        # fig.update_yaxes(tickfont_size=15, ticks="outside")
-        st.plotly_chart(fig, use_container_width=True)
+            fig = graphs20('Sec3THI', 'Section3')
+            st.plotly_chart(fig, use_container_width=True)
+            st.write(
+                'Shows the hourly variation of temperature hunmidity index which is used to estimate the level of heat stress expereinced by '
+                'chickens in section 4 of the poultry house')
